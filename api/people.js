d32ca1f73c8a8db822146a401164f9ca65a989ec
Steve Faulkner <southpolesteve@gmail.com>
2020-03-24 12:25:49 -0500

    WIP

--- api/people.js
+++ api/people.js
@@ -1,116 +1,107 @@
-const parse = require('csv-parse/lib/sync');
-const fetch = require('node-fetch');
+const parse = require("csv-parse/lib/sync");
+const fetch = require("node-fetch");
 
 let DATA_SOURCES = [
-      {
-        "locale": "MSN",
-        "type": "gdoc-1",
-        "uri": "https://docs.google.com/spreadsheets/d/1rS6lfApsy8VzzAeFCKZzfPM8A3R-4GaYj-OOrznceGc/export?format=csv",
-        "format": "csv"
-        "row" : [
-        "_timestamp",
-        "name",
-        "_establishmentUnlisted",
-        "closed",
-        "venmoUser",
-        "paypalEmail",
-        "paypalURL",
-        "establishment",
-        "venmoURL"
-        ]
-      },
-      {
-        "locale": "PGH",
-        "type": "gdoc-1",
-        "uri": "https://docs.google.com/spreadsheets/d/1ggLPsikyyjQw5bJF0zN08nneJoBHA8HlhC0mcnH6fYQ/export?format=csv#gid=1564003411",
-        "format": "csv"
-        "row" : [
-        //"_timestamp",
-        "name",
-        "paypalURL",
-        "establishment",
-        "venmoURL"
-        // "_establishmentUnlisted",
-        // "closed",
-        // "venmoUser",
-        // "paypalEmail",
-        ]
-      }
+  {
+    locale: "MSN",
+    type: "gdoc-1",
+    uri:
+      "https://docs.google.com/spreadsheets/d/1rS6lfApsy8VzzAeFCKZzfPM8A3R-4GaYj-OOrznceGc/export?format=csv",
+    format: "csv",
+    row: [
+      "_timestamp",
+      "name",
+      "_establishmentUnlisted",
+      "closed",
+      "venmoUser",
+      "paypalEmail",
+      "paypalURL",
+      "establishment",
+      "venmoURL"
     ]
+  },
+  {
+    locale: "PGH",
+    type: "gdoc-1",
+    uri:
+      "https://docs.google.com/spreadsheets/d/1ggLPsikyyjQw5bJF0zN08nneJoBHA8HlhC0mcnH6fYQ/export?format=csv#gid=1564003411",
+    format: "csv",
+    row: ["name", "paypalURL", "establishment", "venmoURL"]
+  }
+];
 
 let cache = fetchData(DATA_SOURCES);
-let selected_source=0;
+let selected_source = 0;
 
 function fetchData(sources) {
-    console.log("process.env.env_loaded: ", process.env.env_loaded);
-    console.log("Fetching source", sources[selected_source].uri);
-    return (
-        fetch(sources[selected_source].uri)
-            .then(validateFetch)
-            .then(parseSpreadsheet)
-    );
+  console.log("process.env.env_loaded: ", process.env.env_loaded);
+  console.log("Fetching source", sources[selected_source].uri);
+  return fetch(sources[selected_source].uri)
+    .then(validateFetch)
+    .then(parseSpreadsheet);
 }
 
 function validateFetch(response) {
-    if (!response.ok) {
-        throw new Error(`Failed to fetch ${response.url}: ${response.statusCode}`);
-    }
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${response.url}: ${response.statusCode}`);
+  }
 
-    return response.text();
+  return response.text();
 }
 
 function parseSpreadsheet(spreadsheet) {
-    const rows = parse(spreadsheet).slice(1);
-    return {
-        timestamp: Date.now(),
-        people: rows.map(toRecord)
-    };
+  const rows = parse(spreadsheet).slice(1);
+  return {
+    timestamp: Date.now(),
+    people: rows.map(toRecord)
+  };
 }
 
 function toRecord(row) {
-    const [
-        _timestamp,
-        name,
-        _establishmentUnlisted,
-        closed,
-        venmoUser,
-        paypalEmail,
-        paypalURL,
-        establishment,
-        venmoURL
-    ] = DATASrow;
+  const [
+    _timestamp,
+    name,
+    _establishmentUnlisted,
+    closed,
+    venmoUser,
+    paypalEmail,
+    paypalURL,
+    establishment,
+    venmoURL
+  ] = DATASrow;
 
-    return {
-        name,
-        establishment,
-        haveClosed: closed || null,
-        venmoUser: venmoUser || null,
-        venmoURL: venmoURL || null,
-        paypalUser: paypalEmail || null,
-        paypalURL: paypalURL || null
-    };
+  return {
+    name: row.indexOf("name"),
+    establishment,
+    haveClosed: closed || null,
+    venmoUser: venmoUser || null,
+    venmoURL: venmoURL || null,
+    paypalUser: paypalEmail || null,
+    paypalURL: paypalURL || null
+  };
 }
 
 async function invalidateCache() {
-    try {
-        const data = await fetchData();
-        cache = data;
-    } catch (error) {
-        console.error(`Failed to invalidate cache`, error);
-    }
+  try {
+    const data = await fetchData();
+    cache = data;
+  } catch (error) {
+    console.error(`Failed to invalidate cache`, error);
+  }
 }
 
 module.exports = async (_, res) => {
-    try {
-        const data = await cache;
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(data);
+  try {
+    const data = await cache;
+    res.setHeader("Cache-Control", "s-maxage=300");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(data);
 
-        if (Date.now() - data.timestamp > 1000 * 60 * 60) {
-            invalidateCache();
-        }
-    } catch (error) {
-        res.statusCode = 500;
-        res.send(error.text);
+    if (Date.now() - data.timestamp > 1000 * 60 * 60) {
+      invalidateCache();
     }
+  } catch (error) {
+    res.statusCode = 500;
+    res.send(error.text);
+  }
 };
