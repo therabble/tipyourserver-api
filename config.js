9a0bfc024ad9d566b8f901142d53671ec0778519
prestonaustin <preston.h.austin@gmail.com>
2020-03-25 09:47:27 -0500

    cleanup config mistakes carried over from dev tests

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     locale: "MSN",
-    name: "Madison-like",
+    name: "Madison",
     state: "Wisconsin",
     state_code: "WI",
     country: "USA",
     homepage: "http://bit.ly/MSNTipJar",
     signup: "http://bit.ly/MadisonVirtualTipJar",
     logo: "https://lh3.googleusercontent.com/gsI1DhBOfhdsztJYFwY3Rxf5lU2Ky0gWkjNgqTNhpM1lL1zVib3U1HnowbpKOjm03b-MhTPO8PeHy5tmZEyP7jIt1OHcGhGl-Pcnd7qi6LQ4psKNXbP0UUt_Nf5r2J80MQ",
-    background: "https://homepages.cae.wisc.edu/~ece533/images/mountain.png",
+    background: "greenwindows_small.jpg",
     email: "tipyourserver@therabble.co",
     type: "gdoc-1",
     uri: "https://docs.google.com/spreadsheets/d/1rS6lfApsy8VzzAeFCKZzfPM8A3R-4GaYj-OOrznceGc/export?format=csv",
@@ -23,13 +23,11 @@ module.exports = [
     country: "USA",
     homepage: "http://centerforethicsandpolicy.com/PGHTipJar.html",
     signup: "https://docs.google.com/forms/d/e/1FAIpQLSdTMjsRdW3o3MzZrxE_ibD0TVV6ki-5Vp81qJUAoR1ouU2_cA/viewform",
-    logo: "https://lh3.googleusercontent.com/gsI1DhBOfhdsztJYFwY3Rxf5lU2Ky0gWkjNgqTNhpM1lL1zVib3U1HnowbpKOjm03b-MhTPO8PeHy5tmZEyP7jIt1OHcGhGl-Pcnd7qi6LQ4psKNXbP0UUt_Nf5r2J80MQ",
-    background: "greenwindows_small.jpg",
+    logo: "",
+    background: "http://centerforethicsandpolicy.com/img/PGH11.jpg",
     email: "tipyourserver@therabble.co",
     type: "gdoc-1",
     uri: "https://docs.google.com/spreadsheets/d/1ggLPsikyyjQw5bJF0zN08nneJoBHA8HlhC0mcnH6fYQ/export?format=csv&gid=1564003411",
     format: "csv",
     row: ["name", "paypalURL", "establishment", "venmoURL"]
-  },
-
 ];
